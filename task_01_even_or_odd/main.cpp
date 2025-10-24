3e26ac809c85acc53a61fd8f68e5183de1ac1828
lukaspindiak <lukaspindiak11@gmail.com>
2025-10-24 08:35:05 +0200

    Update main.cpp

--- task_01_even_or_odd/main.cpp
+++ task_01_even_or_odd/main.cpp
@@ -4,7 +4,13 @@ int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     long long n;
-    if (!(std::cin >> n)) return 0;
-    // TODO: print "even" or "odd"
+
+    std::cout << "Nacitaj cislo";
+
+    std::cin >> n;
+
+    if (!(n))
+        return 0;
+
     return 0;
 }
