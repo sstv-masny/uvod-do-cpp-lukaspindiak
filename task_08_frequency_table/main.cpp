9f304a9439dc71738eb61382dfb4138024477e42
LukasPindiak <lukaspindiak11@gmail.com>
2025-11-14 08:35:41 +0100

    update

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -4,6 +4,36 @@
 using namespace std;
 
 int main() {
-    int number[5] = {1, 2, 3, 4, 5};
-    cout << number[2];
+    int number[] = {
+        1,
+        2,
+        3,
+        4,
+        5,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        1,
+        2,
+        3,
+        4,
+        5,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+    };
+
+    int count = sizeof(number) / 4;
+
+    for (int i = 0; i < count; i++) {
+        cout << number[i] << '\n';
+    }
 }
