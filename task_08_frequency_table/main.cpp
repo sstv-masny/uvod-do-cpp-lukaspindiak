29808006a47d95a011aa665be464d23478612270
LukasPindiak <lukaspindiak11@gmail.com>
2025-11-19 14:04:44 +0100

    Update main.cpp

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 int main() {
-    int number[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-    for (int i = 0; i < 11; i++) {
-        cout << number[i] << '\n';
+    int n[101] = {};
+
+    n[1] += 1;
+    n[2] += 1;
+    n[2] += 1;
+    n[100] += 1;
+    n[0] += 1;
+
+    for (int pocet : n) 
+    {
+        cout << pocet << " ";
     }
+    
 }
+
+//  int number[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+//
+//    for (int i = 0; i < 11; i++) {
+//        cout << number[i] << '\n';
+//    }
